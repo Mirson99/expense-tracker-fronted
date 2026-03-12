8fb213a76d418005e40c1227b5c0b65b68a144ae
Mirson99 <lukmir99@gmail.com>
2026-03-12 22:21:39 +0100

    Change backend url

--- src/api/axiosClient.ts
+++ src/api/axiosClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import type { User } from '../types/user';
 
-const BASE_URL = '/api';
+const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
 
 export const axiosClient = axios.create({
   baseURL: BASE_URL,
