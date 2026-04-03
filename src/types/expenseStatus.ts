d454fd2995a86d181bf5db78c832d74ace7bbf6c
Mirson99 <lukmir99@gmail.com>
2026-04-03 14:57:35 +0200

    Fix expense status

--- src/types/expenseStatus.ts
+++ src/types/expenseStatus.ts
@@ -1,6 +1,8 @@
-export enum ExpenseStatus {
-    Confirmed = 0,
-    Processing = 1,
-    RequiresVerification = 2,
-    Failed = 3,
-}
+export const ExpenseStatus = {
+    Confirmed: 0,
+    Processing: 1,
+    RequiresVerification: 2,
+    Failed: 3,
+} as const;
+
+export type ExpenseStatus = typeof ExpenseStatus[keyof typeof ExpenseStatus];
